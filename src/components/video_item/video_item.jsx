7610ce61247be4d82bdee21debe93ff276649ea7
DESKTOP-ROA2184\sujik <sujikim.dev@gmail.com>
2021-08-16 22:11:33 +0900

    유튜브 api 호출 동영상 정보(이미지, 타이틀 등)  출력하기

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import styles from './video_item.module.css'
 
-const VideoItem = (props) => <h1>{props.video}</h1>
+// const VideoItem = (props) => (
+//   <>
+//     <li>
+//       <img src={props.video.thumbnails.medium.url} alt="video thumbnail" />
+//     </li>
+//     <div>
+//       <p>{props.video.title}</p>
+//       <p>{props.video.channelTitle}</p>
+//     </div>
+//   </>
+// )
+
+// props 안에 있는 video를 바로 받아서 사용 -> { }
+const VideoItem = ({ video }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={video.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{video.title}</p>
+        <p className={styles.channel}>{video.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+)
 
 export default VideoItem
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import VideoItem from '../video_item/video_item'
+import styles from './video_list.module.css'
 
 const VideoList = (props) => (
-  <ul>
+  <ul className={styles.video}>
     {/* {props.b.map((video) => ( // 여기에서는 props의 어떤 값인지가 중요.  */}
     {props.videos.map((video) => (
-      <VideoItem key={video.id} video={video.snippet.title} />
+      <VideoItem key={video.id} video={video.snippet} />
     ))}
   </ul>
 )
