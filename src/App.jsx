5b9234be98787829cf7882a1bd4395cb5134b006
DESKTOP-ROA2184\sujik <sujikim.dev@gmail.com>
2021-08-19 21:54:20 +0900

    검색 api 호출해서 동영상 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import SearchHeader from './components/search_header/search_header'
 import VideoList from './components/video_list/video_list'
+import styles from './app.module.css'
 
 function App() {
   //  1. App 컴포넌트가 마운트될 때 비디오 목록 받아오기]
@@ -8,7 +10,26 @@ function App() {
   //  3. 해당 컴포넌트에서 state에서 비디오 목록을 사용하기 위해  useState를 사용한다.
   const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY
 
+  // state 선언 위치 ? (연관된 컴포넌트 중) 최상위 컴포넌트
+  // 리액트는 특정 state가 변경되면 해당 컴포넌트와 그 하위의 컴포넌트 모두 리렌더링됨
   const [videos, setVideos] = useState([])
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&type=video&q=${query}&key=${apiKey}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        // result에 있는 items를 빙글빙글 돌면서 아이템을 그대로 하나하나씩 복사하는데 id는 특정 값으로 덮어씌운다.
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log('error', error))
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -17,8 +38,7 @@ function App() {
     }
 
     fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=' +
-        apiKey,
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${apiKey}`,
       requestOptions
     )
       .then((response) => response.json())
@@ -27,7 +47,12 @@ function App() {
   }, [])
 
   // return <VideoList a={videos.id} b={videos} /> 여기서는 결국 어떤 이름(a, b)으로 넘겨주던지 상관이 없다.
-  return <VideoList key={videos.id} videos={videos} />
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList key={videos.id} videos={videos} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react'
+import styles from './search_header.module.css'
+
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef()
+
+  const handleSearch = () => {
+    const value = inputRef.current.value
+    // 콜백함수 : 어떤 이벤트가 발생했거나 특정 시점에 도달했을 때 시스템에서 호출하는 함수
+    // handleSearch() 실행되면 onSearch() 실행 : 상위컴포넌트의 함수
+    onSearch(value)
+  }
+  const onClick = () => {
+    handleSearch()
+  }
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch()
+    }
+  }
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          className={styles.buttonImg}
+          src="/images/search.png"
+          alt="search"
+        />
+      </button>
+    </header>
+  )
+}
+
+SearchHeader.propTypes = {}
+
+export default SearchHeader
