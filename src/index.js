ef1d649bfb64ce64cb070a422e0821aedaed9a5f
DESKTOP-ROA2184\sujik <sujikim.dev@gmail.com>
2021-08-23 22:29:06 +0900

    유튜브 검색 기능 리팩토링

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Youtube from './service/youtube'
 
+const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY
+const youtube = new Youtube(apiKey) // index.js가 호출 될 때 1번만 생성된다.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 )
